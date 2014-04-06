4f0632236a9aea90b412fe61073b2755225ffa64
jimpea <jimpea@gmail.com>
2014-04-06 21:44:38 +0100

    Up to scripting

---
+++ simpleEx.R
@@ -0,0 +1,11 @@
+# Define a variable
+c <- rnorm(10)
+
+# calculate thr mean of x an print out the results
+mux = mean(x)
+cat("The mean of x is ", mean(x), "\n")
+
+#print out a summary of the results
+summary(x)
+cat("The summary of x is \n", summary(x), "\n")
+print(summary(x))
